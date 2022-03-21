c88aa86e885ff6c177ac3447ccf99e87f970e278
moslem <moslem.ezati@gmail.com>
2022-03-21 12:38:07 +0330

    refactor: Modify writeToFile() to all multiple worksheet to the file

--- src/utils.js
+++ src/utils.js
@@ -59,36 +59,45 @@ function getOscillatorsData(oscillators, movingAverage, summary) {
   return false;
 }
 
-async function writeToFile(pairSimbols, number) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const date = new Date();
-      const time = date.toLocaleTimeString('en-US', { hour12: false });
-      const fileName = date.toLocaleDateString().replace(/\//g, '-') + '.xlsx';
-      filePath = './data/' + fileName;
-      const workbook = new ExcelJS.Workbook();
-      let worksheet;
-      if (fs.existsSync(filePath)) {
-        await workbook.xlsx.readFile(filePath);
-        worksheet = workbook.getWorksheet(pairSimbols);
-        if (!worksheet) {
-          worksheet = workbook.addWorksheet(pairSimbols);
-        }
-      } else {
-        worksheet = workbook.addWorksheet(pairSimbols);
+async function writeToFile(rows) {
+  try {
+    if (!rows || !Array.isArray(rows) || rows.length == 0) {
+      throw new Error('the rows of data is not valid');
+    }
+    const date = new Date();
+    const time = date.toLocaleTimeString('en-US', { hour12: false });
+    const fileName = date.toLocaleDateString().replace(/\//g, '-') + '.xlsx';
+    filePath = './data/' + fileName;
+    const workbook = new ExcelJS.Workbook();
+    let worksheet;
+    if (fs.existsSync(filePath)) {
+      await workbook.xlsx.readFile(filePath);
+    }
+    rows.forEach(function createExcelRow(row) {
+      const sheetName = row.symbol;
+      worksheet = workbook.getWorksheet(sheetName);
+      if (!worksheet) {
+        worksheet = workbook.addWorksheet(sheetName);
       }
       worksheet.columns = [
         { header: 'Time', key: 'time' },
         { header: 'Number', key: 'number' },
       ];
-      worksheet.addRow({ time, number });
-      await workbook.xlsx.writeFile(filePath);
-      resolve(true);
-    } catch (e) {
-      console.log('Error in writeToFile() is', e);
-      reject(false);
-    }
-  });
+      worksheet.addRow({
+        time,
+        number: getOscillatorsData(
+          row.oscillators,
+          row.movingAverage,
+          row.summary
+        ),
+      });
+    });
+    await workbook.xlsx.writeFile(filePath);
+    return true;
+  } catch (e) {
+    console.log('Error in writeToFile() is', e);
+    return false;
+  }
 }
 
 exports.getList = getList;
